0fe3db00ed625f25029e83a35d4d9ca8408bb8b3
imran2759 <ayanimran849@gmail.com>
2025-07-22 12:59:52 +0530

    Start tracking uploads folder

--- dist/banner/banner.resolver.d.ts
+++
@@ -1,7 +0,0 @@
-import { BannerService } from "./banner.service";
-import { Banner } from "./entity/banner.entity";
-export declare class BannerResolver {
-    private bannerService;
-    constructor(bannerService: BannerService);
-    findAll(): Promise<Banner[]>;
-}
--- dist/banner/banner.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from "typeorm";
-import { Banner } from "./entity/banner.entity";
-export declare class BannerService {
-    private bannerRepository;
-    constructor(bannerRepository: Repository<Banner>);
-    findAll(): Promise<Banner[]>;
-}
